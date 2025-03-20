5be7d66471c31a9432b5a4fd7262031d8a3ef7ac
98001/yash <yashchauhan.gaya@gmail.com>
2025-03-20 18:19:56 +0530

    Initial commit - Added Spring Boot project

---
+++ src/test/java/com/company/FoodBridgeApplication/FoodBridge/FoodBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.FoodBridgeApplication.FoodBridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
